ea3302957f1943127ce31feda3c5a8de8c60463f
AlekseyDildikov <adildikov@gmail.com>
2021-08-01 16:32:49 +0300

    finally edit editBook

--- src/Components/Modal/ModalContainer.jsx
+++ src/Components/Modal/ModalContainer.jsx
@@ -32,18 +32,21 @@ export default React.memo(function ModalContainer({
         className="body__input"
         type="text"
         placeholder="Author"
+        value={newBook.author}
         onChange={(e) => onEditNewBook(e, "author")}
       />
       <textarea
         className="body__textarea"
         type="text"
         placeholder="Description of the book"
+        value={newBook.description}
         onChange={(e) => onEditNewBook(e, "description")}
       />
       <input
         className="body__input"
         type="text"
         placeholder="Cover of the book(enter url)"
+        value={newBook.image}
         onChange={(e) => onEditNewBook(e, "image")}
       />
     </Modal>
