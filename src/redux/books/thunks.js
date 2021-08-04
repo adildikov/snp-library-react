cd5a95620c54821a4cf06cbee4efc358e0316df0
AlekseyDildikov <adildikov@gmail.com>
2021-08-04 22:25:59 +0300

    edit thunks

--- src/redux/books/thunks.js
+++ src/redux/books/thunks.js
@@ -8,23 +8,15 @@ import {
 } from "../../Api";
 import { addBook, deleteBook, editBook, setBooks } from "./actions";
 
-export const getBooksThunk = () => (dispatch) => {
-  getBooksRequest(apiUrl).then((data) => {
-    return data;
-  });
-};
-
 export const initBooksThunk = () => (dispatch) => {
   getBooksRequest(apiUrl).then((data) => {
     dispatch(setBooks(data));
-    return data;
   });
 };
 
 export const addBookThunk = (data) => (dispatch) => {
   postBookRequest(apiUrl, data).then((data) => {
     dispatch(addBook(data));
-    return data;
   });
 };
 
@@ -32,7 +24,6 @@ export const deleteBookThunk = (id) => (dispatch) => {
   deleteBookRequest(apiUrl, id).then((response) => {
     dispatch(deleteBook(response));
     dispatch(initBooksThunk());
-    return response;
   });
 };
 
@@ -40,13 +31,11 @@ export const editBookThunk = (id, data) => (dispatch) => {
   editBookRequest(apiUrl, id, data).then((response) => {
     dispatch(editBook(response));
     dispatch(initBooksThunk());
-    return response;
   });
 };
 
 export const getFilteredBooksThunk = (str) => (dispatch) => {
   getFilteredBooksRequest(apiUrl, str).then((data) => {
     dispatch(setBooks(data));
-    return data;
   });
 };
